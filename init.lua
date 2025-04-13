1015927b1e94e4da9137fcfd3735b0d21620d202
CodyPedersen <cody.pedersen@proton.me>
2025-04-13 00:30:31 -0400

    ctrl u ctrl d changes

--- init.lua
+++ init.lua
@@ -1,2 +1,3 @@
 require("cfg-default")
 print("hello")
+
--- lua/cfg-default/remap.lua
+++ lua/cfg-default/remap.lua
@@ -1,5 +1,20 @@
 vim.g.mapleader = " "
+
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+vim.keymap.set('n', '<leader>rt', function()
+  vim.cmd('belowright split | terminal')
+end, { noremap = true, silent = true })
+
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { noremap = true, silent = true })
+
+-- swap windows
+--vim.keymap.set('n', '<M-j>', '<C-w>j', { noremap = true, silent = true })
+--vim.keymap.set('n', '<M-k>', '<C-w>k', { noremap = true, silent = true })
+--vim.keymap.set('n', '<M-h>', '<C-w>h', { noremap = true, silent = true })
+--vim.keymap.set('n', '<M-l>', '<C-w>l', { noremap = true, silent = true })
 
 print("** Configured remap **")
 
