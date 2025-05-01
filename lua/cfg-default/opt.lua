43c37faa2b1f40400afeca7c1f89142cd79ecf43
CodyPedersen <cody.pedersen@proton.me>
2025-05-01 17:24:28 -0400

    GitGutter required configs

--- lua/cfg-default/opt.lua
+++ lua/cfg-default/opt.lua
@@ -2,3 +2,4 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = 4       -- Number of visual spaces per TAB
 vim.opt.shiftwidth = 4    -- Number of spaces for each indentation
 vim.opt.expandtab = true  -- Convert tabs to spaces
+vim.opt.updatetime = 100  -- Speed up modified lines refresh
